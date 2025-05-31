c6e3725b610cd0520e9387d73e64617171100e67
Batyr <awbatyr@gmail.com>
2025-05-31 16:50:59 +0500

    added smoothscroll and main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Outlet } from "react-router-dom";
+import { Footer, Header, SmoothScroll } from "./components/layout";
+
 function App() {
-  return <div></div>;
+  return (
+    <SmoothScroll
+      className="flex flex-col min-h-screen bg-light-bg"
+      data-scroll-container
+    >
+      <Header />
+
+      <main className="flex-auto flex flex-col gap-40">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </SmoothScroll>
+  );
 }
 
 export default App;
