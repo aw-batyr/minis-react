693d4433d2756518ec9cc6e3208d788c6b115e84
Batyr <awbatyr@gmail.com>
2025-05-31 16:53:18 +0500

    socials data

---
+++ src/lib/constantas.ts
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    icon: "/socials/yt.svg",
+    link: "",
+    alt: "youtube",
+  },
+  {
+    icon: "/socials/tt.svg",
+    link: "",
+    alt: "tik-tok",
+  },
+  {
+    icon: "/socials/inst.svg",
+    link: "",
+    alt: "instagram",
+  },
+];
