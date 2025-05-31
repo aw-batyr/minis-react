45683e052189794401d51cdff718b6b2966abd67
Batyr <awbatyr@gmail.com>
2025-05-31 16:52:44 +0500

    reexports

---
+++ src/components/layout/index.ts
@@ -0,0 +1,4 @@
+export { default as SmoothScroll } from "./smooth-scroll";
+export { Header } from "./header";
+export { Footer } from "./footer";
+export { Error } from "./error";
---
+++ src/components/shared/index.ts
@@ -0,0 +1,2 @@
+export { Burger } from "./burger";
+export { AnimatedTitle } from "./animated-title";
