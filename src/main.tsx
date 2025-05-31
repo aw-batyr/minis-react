c7491aea46c12a796d9251d4bc805201a14163d9
Batyr <awbatyr@gmail.com>
2025-05-31 16:51:23 +0500

    added rounting react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,26 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home.tsx";
+import { Error } from "./components/layout";
+
+export const router = createBrowserRouter([
+  {
+    element: <App />,
+    path: "/",
+    children: [
+      {
+        element: <Home />,
+        path: "",
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
