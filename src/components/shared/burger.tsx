a92af5944d46f2b8085fcaeefbeda4b44df493d8
Batyr <awbatyr@gmail.com>
2025-05-31 16:53:01 +0500

    burger icon ready

---
+++ src/components/shared/burger.tsx
@@ -0,0 +1,10 @@
+export const Burger = () => {
+  return (
+    <>
+      <div className="h-[2.08vw] w-[3.64vw] flex flex-col justify-center items-center gap-[0.4vw] cursor-pointer px-4 overflow-hidden">
+        <span className="w-[3.02vw] h-[0.1vw] bg-amber-950"></span>
+        <span className="w-[3.02vw] h-[0.1vw] bg-amber-950"></span>
+      </div>
+    </>
+  );
+};
