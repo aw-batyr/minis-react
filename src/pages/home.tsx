7a746efdbe67d8db15adeaae72fbec7c86915a9a
Batyr <awbatyr@gmail.com>
2025-05-31 16:53:30 +0500

    added sections for home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import { About, Hero, Slogan, Words } from "../components/home";
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <Words />
+      <Slogan />
+      <About />
+    </div>
+  );
+}
