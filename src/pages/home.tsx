0bd7d1a3065321752b917fcaa7f297182e19b767
Batyr <awbatyr@gmail.com>
2025-06-03 20:06:37 +0500

    added products section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,11 @@
-import { About, Hero, Slogan, Words } from "../components/home";
+import { About, Hero, Products, Slogan, Words } from "../components/home";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Words />
+      <Products />
       <Slogan />
       <About />
     </div>
