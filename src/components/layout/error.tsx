81faac3f983eed6df5936ea6d439f3260871d6d2
Batyr <awbatyr@gmail.com>
2025-06-03 20:06:09 +0500

    text size changed

--- src/components/layout/error.tsx
+++ src/components/layout/error.tsx
@@ -1,7 +1,9 @@
 export const Error = () => {
   return (
     <section>
-      <h1 className="text-[10vw] uppercase">Oops, something went wrong :(</h1>
+      <h1 className="text-[10vw] track text-black-bg text-center pt-[5vw] uppercase">
+        Oops, something went wrong :(
+      </h1>
     </section>
   );
 };
