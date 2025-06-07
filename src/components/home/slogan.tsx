c6cc617ef65fbfea58d8b1aae345ef5da48c1d35
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-06-08 04:22:36 +0500

    feat: implement GSAP animations in Slogan component and refactor structure

--- src/components/home/slogan.tsx
+++ src/components/home/slogan.tsx
@@ -1,32 +1,78 @@
-import { AnimatedTitle } from "../shared";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export const Slogan = () => {
+  const containerRef = useRef(null);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline();
+
+      tl.fromTo(
+        ".mask",
+        {
+          autoAlpha: 0,
+          width: 0,
+        },
+        {
+          width: "auto", //Конечное состояние - нормальная ширина
+          autoAlpha: 1,
+          stagger: 0.1,
+          duration: 2,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: containerRef.current,
+            start: "top 60%",
+            end: "bottom 100%",
+            scrub: 1,
+          },
+          willChange: "transform opacity",
+        }
+      );
+    },
+    { scope: containerRef }
+  );
+
   return (
-    <section className="bg-black-bg py-[8.85vw] !text-[5.83vw] flex flex-col items-center -tracking-[0.4vw]">
-      <AnimatedTitle
-        title="Shu yere"
-        textClassName="!text-[5.83vw] !pb-[1vw]"
-        className="px-[4vw] rotate-6 "
-      />
-      <AnimatedTitle
-        title="bize degishli gowja"
-        textClassName="!text-[5.83vw] !pb-[1vw]  !text-[#2E2D2F] leading-1"
-        className="bg-white-text px-[0.7vw]"
-        decorClassName="!bg-black-bg !rotate-[1.75deg] !h-[0.3vw] mb-[0.2vw]"
-        variant={2}
-      />
-      <AnimatedTitle
-        title="slogan yaljak soz"
-        textClassName="!text-[5.83vw]"
-        className="!bg-[#7F3B2D] px-[5vw] rotate-2 z-30"
-        decorClassName="!bg-black-bg"
-        variant={2}
-      />
-      <AnimatedTitle
-        title="gerek bolar"
-        textClassName="!text-[5.83vw] !text-[#2E2D2F]"
-        className="!bg-[#FED775] -rotate-[5deg] mt-[0.5vw] !px-[1.5vw]"
-      />
+    <section
+      ref={containerRef}
+      className="slogan-section relative h-screen bg-black-bg uppercase text-[6vw] flex flex-col items-center -tracking-[0.4vw]"
+    >
+      {/* Заголовок 1 */}
+      <div className="absolute top-[10vw] rotate-6 z-10">
+        <div className="mask inline-block overflow-hidden origin-center">
+          <h2 className="bg-light-brown px-[1vw]">Shu yere</h2>
+        </div>
+      </div>
+
+      {/* Заголовок 2 */}
+      <div className="absolute top-[18vw]">
+        <div className="mask inline-block overflow-hidden origin-center">
+          <h2 className="bg-white-text !text-[#2E2D2F] !px-[2vw]">
+            bize degishli gowja
+          </h2>
+        </div>
+      </div>
+
+      {/* Заголовок 3 */}
+      <div className="absolute bottom-[12.5vw] rotate-2 z-30">
+        <div className="mask inline-block overflow-hidden origin-center">
+          <h2 className="!bg-[#7F3B2D] !px-[7vw]">slogan yaljak soz</h2>
+        </div>
+      </div>
+
+      {/* Заголовок 4 */}
+      <div className="absolute bottom-[4vw] -rotate-[5deg] mt-[0.5vw]">
+        <div className="mask inline-block overflow-hidden origin-center">
+          <h2 className="!bg-[#FED775] !text-[#2E2D2F] !px-[2vw]">
+            gerek bolar
+          </h2>
+        </div>
+      </div>
     </section>
   );
 };
